29281925a5aafc0c8d08b6311e37b07e0470c926
MihirMagikarp <mihir.majgaonkar@gmail.com>
2020-08-25 23:38:08 +0000

    edited print statements

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ i = 0
 
 def hamborger(e):
   for i in range(e):
-    print("H A M B O U R G E R")
+    print("H A M B O U R G E R\n")
 
 def hotdoge(n):
   global i
 
   while True:
-    print("H O T D O G E")
+    print("H O T D O G E\n")
     i += 1
 
     if i == n:
       break
 
 hamborger(3)
+hotdoge(21)
