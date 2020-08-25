68826b07e4414f6c5ddbbeff3fdc05beca853a03
MihirMagikarp <mihir.majgaonkar@gmail.com>
2020-08-25 23:37:22 +0000

    added hotdoge() function

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+i = 0
+
 def hamborger(e):
   for i in range(e):
     print("H A M B O U R G E R")
+
+def hotdoge(n):
+  global i
+
+  while True:
+    print("H O T D O G E")
+    i += 1
+
+    if i == n:
+      break
 
 hamborger(3)
